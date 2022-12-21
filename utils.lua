319435f9fc91507946568e98e7884df7b77377b1
Волчачка Беловолк <54104335+volchachka@users.noreply.github.com>
2022-12-21 16:43:45 +0400

    utils.lua -_-

---
+++ utils.lua
@@ -0,0 +1,34 @@
+function ToHex(v)
+
+    local hex = "0123456789ABCDEF"
+    local z = v
+    local m = 0
+    local str = ""
+    
+    if z == 0 then return "0" end
+        
+    while z > 0 do
+        m = ( z % 16 ) + 1
+        z = math.floor(z / 16)
+        str = string.sub(hex,m,m) .. str
+    end
+    
+    return str
+end
+
+
+function RV2I(r)
+   if string.len(r) == 4  then
+       local int = string.byte(r,4,4) + string.byte(r,3,3) * 256 + string.byte(r,2,2) * 65536 + string.byte(r,1,1) * 16777216
+       return int
+   end
+    return 0
+end
+
+function RV2X(r)
+    if string.len(r) == 4  then
+        local int = string.byte(r,4,4) + string.byte(r,3,3) * 256 + string.byte(r,2,2) * 65536 + string.byte(r,1,1) * 16777216
+        return ToHex(r)
+    end
+     return 0
+end
